9f5238a518cf312720556fda1eb0c045cc90712c
Denis Arruda Santos <denis.arruda@gmail.com>
2026-02-24 20:46:52 -0300

    listtodos function

---
+++ todos/listTodos.mjs
@@ -0,0 +1,28 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb"; // ES Modules import
+// const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb"); // CommonJS import
+// import type { DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+const config = {}; // type is DynamoDBClientConfig
+const client = new DynamoDBClient(config);
+
+export const handler = async (event) => {
+  //const id = event.pathParameters.id
+
+  const input = {
+    FilterExpression: "username = :value",
+    ExpressionAttributeValues: { ":value": { S: "in28minutes" } },
+    TableName: "todo"
+  };
+
+  const command = new ScanCommand(input);
+  const results = await client.send(command);
+  const headers = {
+    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+  }
+  
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(results.Items),
+    headers
+  };
+  return response;
+};
